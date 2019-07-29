2928aca15995da305710e281b347e3f68302d335
marjorii <hello@marjorieober.com>
2019-07-29 22:12:51 +0200

    Set default page and apply to other pages. Prepare blueprints and add JS

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// resize menu bar (and mail address div)
+function resizeBar() {
+    var bar = document.querySelector('.menu-bar');
+    // var mail = document.getElementById('mail-address');
+    var currentWidth = document.querySelector('section.right').offsetWidth;
+    var finalWidth = currentWidth - 20;
+    var portraitTablet = window.matchMedia("(min-width: 768px) and (max-height: 1366px) and (orientation: portrait)").matches;
+    var isPortraitTablet = false;
+    bar.style.cssText = 'width: ' + finalWidth + 'px';
+    // mail.style.cssText = 'width: ' + finalWidth + 'px';
+
+    if (window.location.pathname == '/home/marjorie/dev/++=/project.html' && portraitTablet) {
+        isPortraitTablet = true;
+        currentWidth = document.querySelector('#grid-container > div:last-of-type').offsetWidth;
+        bar.style.cssText = 'width: ' + currentWidth + 'px';
+        // mail.style.cssText = 'width: ' + currentWidth + 'px';
+    }
+    else {
+        isPortraitTablet = false;
+    }
+    var isTactile = false;
+    if ( /Android|webOS|iPhone|iPad|Kindle|Tablet|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        isTactile = true;
+        bar.style.cssText = 'width: ' + currentWidth + 'px';
+        // mail.style.cssText = 'width: ' + currentWidth + 'px';
+    }
+    else {
+        isTactile = false;
+    }
+}
+resizeBar();
+window.addEventListener('resize', resizeBar);
